d114abdbf582cfea55d39495971e096a090511c2
6nb <77910109+6nb@users.noreply.github.com>
2021-12-27 13:53:31 -0500

    Update example.js
    killed link anyway

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 // NodeJS example with Request
 const request = require('request');
 
-const URL = 'https://docs.google.com/forms/d/e/1FAIpQLScv38vvS_ws0HNtDIGdEJ487tNyuQ71Dxb826Jpxh1DD10GDg/formResponse';
+const URL = 'https://docs.google.com/forms/.../formResponse';
 const DATA = {
     'entry.0123456789':'Answer 1', // Answer for Question 1
     'entry.1234567890':'javascript text entry' // Answer for Question 2
